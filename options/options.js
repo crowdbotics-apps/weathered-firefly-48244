9f1dcb441231748827c2c7ad8a75a32d4c890c2d
Crowdbotics <team@crowdbotics.com>
2024-06-03 18:12:27 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "weathered-firefly-48244",
+  url: "https://weathered-firefly-48244.botics.co",
+  api: "https://weathered-firefly-48244.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
